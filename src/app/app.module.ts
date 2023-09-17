5005b1dc5f169c03bad4dacb7dbfaf7e219dcfc3
ana <anapaulavieira.ac@gmail.com>
2023-09-17 10:04:56 -0500

    showing dialog buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConfirmDialogModule } from './components/confirm-dialog/confirm-dialog.module';
+import { DialogModule } from '@angular/cdk/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ConfirmDialogModule,
+    DialogModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
