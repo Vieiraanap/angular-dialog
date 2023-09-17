b0e5332f817b8c5c8baff84466f03e8094c3d5d6
ana <anapaulavieira.ac@gmail.com>
2023-09-17 10:04:41 -0500

    showing dialog buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogData } from './models/dialog-data';
+import { DialogSizeEnum } from './models/enum/dialog-size.enum.enum';
+import { DialogComponent } from './components/dialog/dialog.component';
+import { ConfirmDialogService } from './components/confirm-dialog/confirm-dialog.service';
+import { take, switchMap, EMPTY } from 'rxjs';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-dialog';
+
+  constructor(
+    public dialog: MatDialog,
+    private confirmDialogService: ConfirmDialogService,
+    private dataService: DataService
+  ) {}
+
+  openDialog(dialogSize: DialogSizeEnum): void {
+    let dialogData = new DialogData<string>({
+      title: dialogSize.toUpperCase(),
+      size: dialogSize,
+      object: `This is a ${dialogSize} dialog`
+    });
+
+    this.dialog.open(DialogComponent, {
+			data: dialogData,
+		});
+  }
+
+  openConfirmDialog(): void {
+    const confirm$ = this.confirmDialogService.openConfirmDialog();
+    confirm$.asObservable().pipe(
+      take(1),
+      switchMap(confirm => confirm ? this.dataService.getData() : EMPTY)
+    ).subscribe(
+      (data) => alert(data)
+    );
+  }
+
+  get dialogSize() {
+    return DialogSizeEnum;
+  }
 }
